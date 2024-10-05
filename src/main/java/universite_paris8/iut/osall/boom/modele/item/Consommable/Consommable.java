e0612e703ffb3dd5cde1e09c408b01ea9a6f0acf
Alex <apham@iut.univ-paris8.fr>
2024-10-05 23:27:53 +0200

    refactoring de consommable et de ses sous-class

--- src/main/java/universite_paris8/iut/osall/boom/modele/item/Consommable/Consommable.java
+++ src/main/java/universite_paris8/iut/osall/boom/modele/item/Consommable/Consommable.java
@@ -1,16 +1,38 @@
 package universite_paris8.iut.osall.boom.modele.item.Consommable;
 
-import universite_paris8.iut.osall.boom.modele.Environnement.Environnement;
+import universite_paris8.iut.osall.boom.modele.entite.Joueur;
 import universite_paris8.iut.osall.boom.modele.item.Item;
 
 public abstract class Consommable extends Item {
-    public Consommable(Environnement environnement, String nom, int x, int y) {
-        super(environnement, nom, x, y);
+    protected Joueur joueur;
+
+    public Consommable(Joueur joueur, String nom, int x, int y) {
+        super(joueur.getEnvironnement(), nom, x, y);
+        this.joueur = joueur;
     }
 
-    public Consommable(Environnement environnement, String nom) {
-        super(environnement, nom);
+    public Consommable(Joueur joueur, String nom) {
+        super(joueur.getEnvironnement(), nom);
+        this.joueur = joueur;
     }
 
     public abstract void utilise();
+
+    protected void soin(int valeurSoin, int limitePvMax) {
+        if (joueur.getPv() + valeurSoin <= limitePvMax) {
+            joueur.rajouterPv(valeurSoin);
+        } else {
+            joueur.setPv(limitePvMax);
+        }
+        retirerDeLInventaire();
+    }
+
+    public void fullSoin() {
+        joueur.setPv(joueur.getPvMax());
+        retirerDeLInventaire();
+    }
+
+    private void retirerDeLInventaire() {
+        joueur.getInventaire().remove(this);
+    }
 }
--- src/main/java/universite_paris8/iut/osall/boom/modele/item/Consommable/PotionHeal.java
+++ src/main/java/universite_paris8/iut/osall/boom/modele/item/Consommable/PotionHeal.java
@@ -2,28 +2,20 @@ package universite_paris8.iut.osall.boom.modele.item.Consommable;
 
 import universite_paris8.iut.osall.boom.modele.entite.Joueur;
 
-public class PotionHeal extends Consommable{
-
-    private Joueur joueur;
+public class PotionHeal extends Consommable {
+    private static final int val_soin = 90;
+    private static final int val_pvMax = 300;
 
     public PotionHeal(Joueur joueur) {
-        super(joueur.getEnvironnement(), "Potion de Soin");
-        this.joueur = joueur;
+        super(joueur, "Potion de Soin");
     }
 
     public PotionHeal(Joueur joueur, int x, int y) {
-        super(joueur.getEnvironnement(), "Potion de Soin", x, y);
-        this.joueur = joueur;
+        super(joueur, "Potion de Soin", x, y);
     }
 
-    public void utilise(){
-        seSoigne();
-    }
-
-    public void seSoigne(){
-        if (joueur.getPv() + 90 <= 300){
-            joueur.rajouterPv(90);
-            this.joueur.getInventaire().remove(this);
-        }
+    @Override
+    public void utilise() {
+        soin(val_soin, val_pvMax);
     }
 }
