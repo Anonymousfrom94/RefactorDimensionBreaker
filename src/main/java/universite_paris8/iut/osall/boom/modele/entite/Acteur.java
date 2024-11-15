044a19254a8248c13c023171d5bebb3debb4e66a
ylogbi <logbi.younes@outlook.com>
2024-11-15 14:41:39 +0100

    changemnt import

--- src/main/java/universite_paris8/iut/osall/boom/modele/entite/Acteur.java
+++ src/main/java/universite_paris8/iut/osall/boom/modele/entite/Acteur.java
@@ -1,9 +1,6 @@
 package universite_paris8.iut.osall.boom.modele.entite;
 
 import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
 import universite_paris8.iut.osall.boom.modele.Utilitaire.PV;
 import universite_paris8.iut.osall.boom.modele.Environnement.Environnement;
 import universite_paris8.iut.osall.boom.modele.Utilitaire.Direction;
